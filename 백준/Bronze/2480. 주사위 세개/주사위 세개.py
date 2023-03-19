685d6becad3345dd3bab261596db88fc207e9828
YongWan Yun <89889583+ayocado@users.noreply.github.com>
2023-03-20 00:03:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,15 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+if a == b and b == c:
+    print(10000 + a * 1000)
+elif a != b and b != c and c != a:
+    print(max(a, b, c) * 100)
+else:
+    if a == b:
+        print(1000 + a * 100)
+    elif b == c:
+        print(1000 + b * 100)
+    else:
+        print(1000 + c * 100)
