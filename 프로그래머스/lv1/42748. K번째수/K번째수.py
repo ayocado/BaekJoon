6d97a16eda216b1d4a48d2373145d11bb53bcefd
YongWan Yun <89889583+ayocado@users.noreply.github.com>
2023-07-12 14:49:30 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,11 @@
+# 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        cut = array[command[0] - 1 : command[1]]
+        cut.sort()
+        answer.append(cut[command[2] - 1])
+        
+    return answer
