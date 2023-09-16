b2abcfa9d1d6f4a7f193da79245ff34d0ba28cd9
YongWan Yun <89889583+ayocado@users.noreply.github.com>
2023-09-16 20:19:00 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,23 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+s = []
+
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(1, N+1):
+        if len(s) == 0:
+            s.append(i)
+            dfs()
+            s.pop()
+        else:
+            if s[-1] <= i:
+                s.append(i)
+                dfs()
+                s.pop()
+
+dfs()
