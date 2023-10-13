1e87312042b55bd458835572fa8d22a19899cf4c
YongWan Yun <89889583+ayocado@users.noreply.github.com>
2023-10-13 20:58:18 +0900

    [Silver II] Title: 에디터, Time: 788 ms, Memory: 105004 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,31 @@
+import sys
+
+string = list(map(str, sys.stdin.readline().rstrip()))
+M = int(sys.stdin.readline().rstrip())
+orders = []
+for _ in range(M):
+    orders.append(list(map(str, sys.stdin.readline().split())))
+
+stack = []
+for order in orders:
+    if len(order) == 1:
+        if order[0] == 'L':
+            if len(string) == 0:
+                continue
+            else:
+                stack.append(string.pop())
+        elif order[0] == 'D':
+            if len(stack) == 0:
+                continue
+            else:
+                string.append(stack.pop())
+        elif order[0] == 'B':
+            if len(string) == 0:
+                continue
+            else:
+                string.pop()
+    else:
+        string.append(order[1])
+
+stack.reverse()
+print(''.join(string) + ''.join(stack))
